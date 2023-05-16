a3bcafb68a36dc92ea7ed3e4eb6b446e9415d349
worker <>
2023-05-16 16:25:52 +0800

    超时强踢

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -49,18 +50,14 @@ func (this *Server) BroadCast(user *User, msg string) {
 
 //(this *Server) 与Server进行绑定（内部方法）
 //处理业务
-func (this *Server) Handler(conn net.Conn) {
+func (this *Server) UserHandler(conn net.Conn) {
 	//fmt.Println("链接建立成功")
 	//用户上线，将用户加入onlineMap中
 	user := NewUser(conn, this)
 	user.Online()
+	//监听用户是否活跃
+	isAlive := make(chan bool)
 	//接受客户端发送的消息
-	go this.ReadClientMessage(conn, user)
-	//阻塞
-	select {}
-}
-
-func (this *Server) ReadClientMessage(conn net.Conn, user *User) {
 	go func() {
 		buf := make([]byte, 4096)
 		for {
@@ -77,8 +74,34 @@ func (this *Server) ReadClientMessage(conn net.Conn, user *User) {
 			msg := string(buf[:read-1])
 			//广播消息
 			user.DoMessage(msg)
+			//用户的任何消息都会刷新活跃状态
+			isAlive <- true
 		}
 	}()
+	//阻塞
+	for {
+		//两个case都会触发，如果活跃，定时器的case会触发而内容体不会执行
+		select {
+		case <-isAlive:
+		//当前用户是活跃的，需要重置定时器
+		//不需要做任何动作，为了激活select，更新下面的定时器
+		case <-time.After(time.Second * 10):
+			//已经超时，将当前用户强制踢掉
+			user.sendMsg("超时被踢")
+			//关闭资源
+			close(user.C)
+			//delete(this.OnlineMap,user.Name)//不需要删除，有检测在线状态方法会删除
+			//关闭链接
+			conn.Close()
+			//退出当前handler
+			return //runtime.Goexit()
+
+		}
+	}
+}
+
+func (this *Server) ReadClientMessage(conn net.Conn, user *User) {
+
 }
 
 func (this *Server) Start() {
@@ -101,6 +124,6 @@ func (this *Server) Start() {
 			continue
 		}
 		//do handler
-		go this.Handler(conn) //异步处理业务
+		go this.UserHandler(conn) //异步处理业务
 	}
 }
